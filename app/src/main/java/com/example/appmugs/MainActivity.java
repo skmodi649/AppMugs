d3f724c314d8625bf86a18d8bbe363d47ad4711c
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-06-04 12:29:29 +0530

    Login screen designing commit

--- app/src/main/java/com/example/appmugs/MainActivity.java
+++ app/src/main/java/com/example/appmugs/MainActivity.java
@@ -3,9 +3,9 @@ package com.example.appmugs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
