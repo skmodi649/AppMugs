2373c93c3516ced924e00f20a260f1242342426b
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-06-04 12:37:59 +0530

    Created new tab for bottom tab navigation

---
+++ app/src/main/java/com/example/appmugs/Bottom_Tabs.java
@@ -0,0 +1,14 @@
+package com.example.appmugs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bottom_Tabs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_tabs);
+    }
+}
--- app/src/main/java/com/example/appmugs/MainActivity.java
+++ app/src/main/java/com/example/appmugs/MainActivity.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    // Defining the editTexts and progress bars used in the login activity
+    private EditText editTextEmail , editTextPassword;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Assigning ids to the view and progress bars
+
+        editTextEmail = findViewById(R.id.username);
+        editTextPassword = findViewById(R.id.password);
+
+        progressBar = findViewById(R.id.progressBar);  //initialising the progress bar
+
+        mAuth = FirebaseAuth.getInstance();
     }
 }
